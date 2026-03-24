ad3518be4c6b1aaec8b7d061af24cf8661cbcd44
Ajay-137 <ajaymg137@gmail.com>
2026-03-24 16:04:39 +0530

    BRavo

--- public/mailer.php
+++ public/mailer.php
@@ -10,14 +10,14 @@ $mail = new PHPMailer(true);
 
 try {
     $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
+    $mail->Host       = 'smtp-relay.brevo.com';
     $mail->SMTPAuth   = true;
-    $mail->Username   = MAIL_USERNAME;
-    $mail->Password   = MAIL_PASSWORD;
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;  // Changed from STARTTLS to SSL
-    $mail->Port       = 465;                           // Changed from 587 to 465
+    $mail->Username   = getenv('BREVO_SMTP_USER');
+    $mail->Password   = getenv('BREVO_SMTP_PASSWORD');
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
 
-    $mail->setFrom(MAIL_USERNAME, 'Bus App');
+    $mail->setFrom(getenv('BREVO_SMTP_USER'), 'Bus App');
     $mail->isHTML(true);
 } catch (Exception $e) {
     error_log("Mailer setup error: " . $e->getMessage());
